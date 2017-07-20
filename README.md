49995e56574c5a70fa706219b52f07c0343fd1f3
Jorijn Schrijvershof <jorijn@jorijn.com>
2017-07-20 20:28:33 +0200

    more work

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 [![Total Downloads](https://poser.pugx.org/jorijn/laravel-security-checker/downloads)](https://packagist.org/packages/jorijn/laravel-security-checker)
 [![Latest Unstable Version](https://poser.pugx.org/jorijn/laravel-security-checker/v/unstable)](//packagist.org/packages/jorijn/laravel-security-checker)
 [![License](https://poser.pugx.org/jorijn/laravel-security-checker/license)](https://packagist.org/packages/jorijn/laravel-security-checker)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/?branch=master)
+[![Build Status](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/badges/build.png?b=master)](https://scrutinizer-ci.com/g/Jorijn/laravel-security-checker/build-status/master)
 
 ```php
 /**
---
+++ src/ServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Jorijn\LaravelSecurityChecker;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/laravel-security-checker.php';
+        $this->mergeConfigFrom($configPath, 'laravel-security-checker');
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/laravel-security-checker.php';
+        $this->publishes([$configPath => $this->getConfigPath()], 'config');
+    }
+
+    /**
+     * Get the config path
+     *
+     * @return string
+     */
+    protected function getConfigPath()
+    {
+        return config_path('laravel-security-checker.php');
+    }
+
+    /**
+     * Publish the config file
+     *
+     * @param  string $configPath
+     */
+    protected function publishConfig($configPath)
+    {
+        $this->publishes([$configPath => config_path('laravel-security-checker.php')], 'config');
+    }
+}
