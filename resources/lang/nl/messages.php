2f01166c3e39a0d5af0faad8a4d04514257078e4
Mijndert Stuij <mijndert@mijndertstuij.nl>
2017-07-22 16:25:16 +0200

    Added NL language to resources

---
+++ resources/lang/nl/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Security Checker
+    |--------------------------------------------------------------------------
+    |
+    | This file will load all the translations for this package.
+    |
+    */
+
+    'subject_new_vulnerabilities' => '{0} Er zijn geen pakketten met bekende kwetsbaarheden|{1} 1 pakket heeft bekende kwetsbaarheden|[2,*] :count pakketten hebben bekende kwetsbaarheden',
+    'title' => 'Titel',
+    'view' => 'Bekijken',
+    'cve' => 'CVE',
+    'information' => 'Informatie',
+    'no_recipients_configured' => 'Er zijn nog geen ontvangers geconfigureerd!',
+    'body_no_vulnerabilities' => 'Het lijkt erop dat er geen pakketten zijn met bekende kwetsbaarheden. Lees [hier](https://github.com/jorijn/laravel-security-checker#configuration) hoe je dit bericht uit kunt schakelen.'
+];
