5e7124364625e1c122d936a7f230c65b5738a147
Pacho <28653641+gfmr806@users.noreply.github.com>
2021-01-26 17:24:59 -0500

    Lang Spanish - Español
    Traducción a español de los mensajes del paquete.

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Security Checker
+    |--------------------------------------------------------------------------
+    |
+    | Este archivo cargará todas las traducciones de este paquete.
+    |
+    */
+
+    'subject_new_vulnerabilities' => '{0} Ningún paquete tiene vulnerabilidades conocidas | {1} 1 paquete tiene vulnerabilidades conocidas | [2, *]: recuento de paquetes tienen vulnerabilidades conocidas',
+    'title' => 'Titulo',
+    'view' => 'Vista',
+    'cve' => 'CVE',
+    'information' => 'Información',
+    'no_recipients_configured' => 'Aún no se ha configurado ningún destinatario.!',
+    'body_no_vulnerabilities' => 'Parece que ningún paquete tiene vulnerabilidades conocidas. Puede leer más sobre cómo deshabilitar este mensaje [aquí](https://github.com/jorijn/laravel-security-checker#configuration).'
+];
