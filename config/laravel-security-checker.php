27ed8af20042fd787edf2ca93b5313b47f7c5dfe
Thomas de Roo <thomasderoo4@hotmail.com>
2021-03-09 11:14:27 +0100

    Added 'temp_dir' config entry (#36)
    * Added 'temp_dir' config entry
    
     To comply with enlightn/security-checker v1.7.
    
    * Revert "Added 'temp_dir' config entry"
    
    This reverts commit 622ca964d0102c4c60ab4b7ee5ec2f10f6abef20.
    
    * Added temp dir option the right way using dependency injection

--- config/laravel-security-checker.php
+++ config/laravel-security-checker.php
@@ -36,4 +36,19 @@ return [
      */
 
     'slack_webhook_url' => env('LCS_SLACK_WEBHOOK', null),
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel Security Checker — Temp dir
+     |--------------------------------------------------------------------------
+     |
+     | Decides where enlightn/security-checker will place its temp files. 
+     | Useful when using this package with multiple users/permissions on a single server.
+     | See: https://github.com/enlightn/security-checker/issues/17
+     |      https://github.com/Jorijn/laravel-security-checker/issues/35
+     | Value:
+     |   An absolute path to a directory to place the temp files in.
+     |   null = default /tmp directory
+     |
+     */
+    'temp_dir' => null
 ];
